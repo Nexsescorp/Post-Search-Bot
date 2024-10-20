6a49efadff0ddbc853317576e033aa6cfb3e7209
Nexsescorp <ashusingh881062@gmail.com>
2024-10-20 18:39:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,11 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = 
-ADMIN        = 
-CHANNEL      = "RahulReviewsYT"
+import os
+from os import environ
+
+API_ID       = int(environ.get("API_ID", ""))
+API_HASH     = environ.get("API_HASH", "")
+BOT_TOKEN    = environ.get("BOT_TOKEN", "")
+DATABASE_URI = environ.get("DATABASE_URI", "")
+LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", ""))
+ADMIN        = int(environ.get("ADMIN", ""))
+CHANNEL      = environ.get("CHANNEL", "")
+SESSION      = environ.get("SESSION", "")
